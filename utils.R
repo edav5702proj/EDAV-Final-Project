21cd0121e73aaf489924d9b2324185fbb7de6a9f
xysong <xysong25@gmail.com>
2022-10-27 23:06:41 -0400

    Update .gitignore & Create helper for quick build

---
+++ utils.R
@@ -0,0 +1,9 @@
+# Quick render: only work for same-level .rmd file
+# fname can be any .Rmd file
+quick_build <- function(fname){
+  name = sprintf("%s.Rmd", fname)
+  bookdown::render_book(name)
+  browseURL(sprintf("docs/%s.html", fname))
+}
+# Run this to test (quick build helper)
+quick_build("index")
