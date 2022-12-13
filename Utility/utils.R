7dbdbfccd8feea4be0a74e7981b6c8e58c12d9d5
xysong <xysong25@gmail.com>
2022-12-13 16:28:17 -0500

    get attraction name done

--- Utility/utils.R
+++ Utility/utils.R
@@ -10,45 +10,15 @@ quick_build("index")
 
 # Utility function for plotting
 # library(reticulate) #nolint
-ATTRACTION_NAMES <- c(
-  "country bears",
-  "7 dwarfs train",
-  "pirates of caribbean",
-  "astro orbiter",
-  "laugh floor",
-  "regal carrousel",
-  "big thunder mtn",
-  "splash mountain",
-  "hall of presidents",
-  "space mountain",
-  "sorcerers of the mk",
-  "jungle cruise",
-  "mad tea party",
-  "princess hall cinderella elena",
-  "dumbo",
-  "tom land speedway",
-  "swiss family tree",
-  "magic carpets",
-  "tom sawyer island",
-  "peoplemover",
-  "philharmagic",
-  "it s a small world",
-  "town sq mickey",
-  "carousel of progress",
-  "under the sea",
-  "barnstormer",
-  "winnie the pooh",
-  "enchanted tiki rm",
-  "princess hall rapunzel tiana",
-  "pirate s adventure",
-  "liberty sq riverboat",
-  "peter pan s flight",
-  "haunted mansion",
-  "buzz lightyear"
-)
 process_name <- function(csv){
   return(gsub("  ", "", gsub(".csv", "", gsub("_", " ", csv))))
 }
+
+get_attraction_name <- function(){
+  return(lapply(list.files("Data/data/Magic Kingdom/xysong_python"), process_name)) #nolint
+}
+# get_attraction_name()
+
 
 read_all <- function() {
   library(hash)
