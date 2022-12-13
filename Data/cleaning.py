0862a08ba0525065b5f28b3d25ec406484a8e755
xysong <xysong25@gmail.com>
2022-12-13 00:38:57 -0500

    update cleaning.py (fix bugs)

--- Data/cleaning.py
+++ Data/cleaning.py
@@ -28,6 +28,12 @@ def rename_cols(raw: np.ndarray):
 def preprocess(path, format='%Y-%m-%d %H:%M:%S'):
     df = pd.read_csv(path)
     # Check with NAs
+    # Step 1: remove all rows where there are two NAs
+    sactmin = np.array(df['SACTMIN'].notnull())
+    spostmin = np.array(df['SPOSTMIN'].notnull())
+    mask = np.logical_or(sactmin, spostmin)
+    df = df[mask]
+    # Step 2: fill with 0 (this is a trick)
     df = df.fillna(0)
     raw = np.array(df)
     # ic(raw.shape)
@@ -68,23 +74,23 @@ if __name__ == "__main__":
     ic("Data Cleaning & Joining...")
     # Some examples
     # filename = "dumbo"
-    # filename = "7_dwarfs_train"
-    # local_path = "Data/data/Magic Kingdom/" + f"{filename}.csv"
-    # # Convert to dataframe
-    # df = pd.read_csv(local_path)
-    # ic(df.head())
+    filename = "7_dwarfs_train"
+    local_path = "Data/data/Magic Kingdom/" + f"{filename}.csv"
+    # Convert to dataframe
+    df = pd.read_csv(local_path)
+    ic(df.head())
 
-    # eg_date = df.head()['datetime'][0]
-    # ic(eg_date)
-    # format = '%Y-%m-%d %H:%M:%S'  # The format
-    # datetime_str = datetime.datetime.strptime(eg_date, format)
-    # ic(datetime_str.hour)
+    eg_date = df.head()['datetime'][0]
+    ic(eg_date)
+    format = '%Y-%m-%d %H:%M:%S'  # The format
+    datetime_str = datetime.datetime.strptime(eg_date, format)
+    ic(datetime_str.hour)
 
-    # # Using preprocess function
-    # data = preprocess(local_path, format)
-    # ic(data.shape)
+    # Using preprocess function
+    data = preprocess(local_path, format)
+    ic(data.shape)
 
     # ACTUAL PROCESSING CODE (ABOVE ARE ALL EXAMPLES: can comment out if needed)
-    folder_name = "xysong_python"
-    destination = "Data/data/Magic Kingdom/" + f"{folder_name}/"
-    clean_all(destination)
+    # folder_name = "xysong_python"
+    # destination = "Data/data/Magic Kingdom/" + f"{folder_name}/"
+    # clean_all(destination)
