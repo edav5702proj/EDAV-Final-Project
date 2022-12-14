9c5a287e3d45f9f01d04b9f206bb2d04f0f61483
xysong <xysong25@gmail.com>
2022-12-14 16:14:03 -0500

    sync wait time csv

--- Utility/utils.py
+++ Utility/utils.py
@@ -25,8 +25,8 @@ def get_all_csv_names():
     csvs = os.listdir(local_path)
     # ic(csvs[0][:-4])
     csvs = np.array(list(filterfalse(lambda x: x[-4:] != '.csv', csvs)))
-    ic(type(csvs))
-    ic(f"Number of csv files: {csvs.shape}")
+    # ic(type(csvs))
+    # ic(f"Number of csv files: {csvs.shape}")
     # ic(csvs)
     # Process names
     attractions = np.array([x[:-4] for x in csvs])
@@ -46,13 +46,37 @@ def find_num_matched(names, meta):
 
 def parse_attraction_name():
     csv, raw = get_all_csv_names()
-    ic(raw)
-    ic(raw[0])
+    # ic(raw)
+    # ic(raw[0])
     # Parsing
     names = [x.replace("_", " ") for x in raw]
     names = [x.replace("  ", " ") for x in names]
-    ic(names)
+    # ic(names)
+    return names
 
+
+def read_avg_wait():
+    local_path = r"Data/data/entities.csv"
+    df = pd.read_csv(local_path)
+    df = np.array(df.fillna(0))
+    # ic(df)
+    name_duration = {}
+    for item in df:
+        name_duration[item[2].replace("_", " ").replace(
+            "  ", " ").lower()] = item[-1]
+    ic(name_duration)
+
+    lst = []
+    for name in parse_attraction_name():
+        ic(name)
+        if name in name_duration:
+            lst.append([name, name_duration[name]])
+    # ic(lst)
+    # ic(len(lst))
+    # Save to txt file
+    df = pd.DataFrame(np.array(lst), columns=["name", "avg_wait_time"])
+    dest = "Data/data/name_avg_wait.csv"
+    df.to_csv(dest)
 
 
 if __name__ == '__main__':
@@ -66,4 +90,5 @@ if __name__ == '__main__':
     # Find number of matched data
     # find_num_matched(names, meta_names)
 
-    parse_attraction_name()
+    # parse_attraction_name()
+    read_avg_wait()
