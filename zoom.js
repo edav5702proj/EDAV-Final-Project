4b9bb4815d859343938b63e3f491851372ecf9ac
xysong <xysong25@gmail.com>
2022-12-14 15:38:52 -0500

    udpate dataset

--- zoom.js
+++ zoom.js
@@ -13,3 +13,25 @@ d3.select(event.currentTarget).transition().duration(100).attr("width", parseFlo
       state=true;
   }
 });
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var allimg = document.getElementsByClassName("abnormal");
+var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+for (let i = 0; i < 25; i++){
+  allimg[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    // captionText.innerHTML = this.alt;
+  }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
