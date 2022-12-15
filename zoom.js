f192bfd312a3ba94299b5c0b9f4b38779754a1f0
xysong <xysong25@gmail.com>
2022-12-15 01:26:52 -0500

    faceting plot done

--- zoom.js
+++ zoom.js
@@ -1,18 +1,18 @@
-var state=true;
-d3.select("div#facet").selectAll("img")
-  .on("click", function(event) {
-    console.log("hello");
-      console.log(state);
-      var width = parseFloat(d3.select(event.currentTarget).attr("width"))/100;
-      console.log(width);
-  if(state==true){
-d3.select(event.currentTarget).transition().duration(100).attr("width", parseFloat(width*2*100).toFixed(2)+"%");
-      state=false;
-  }else{
-d3.select(event.currentTarget).transition().duration(100).attr("width", parseFloat(width*0.5*100).toFixed(0.5)+"%");
-      state=true;
-  }
-});
+// var state=true;
+// d3.select("div#facet").selectAll("img")
+//   .on("click", function(event) {
+//     console.log("hello");
+//       console.log(state);
+//       var width = parseFloat(d3.select(event.currentTarget).attr("width"))/100;
+//       console.log(width);
+//   if(state==true){
+// d3.select(event.currentTarget).transition().duration(100).attr("width", parseFloat(width*2*100).toFixed(2)+"%");
+//       state=false;
+//   }else{
+// d3.select(event.currentTarget).transition().duration(100).attr("width", parseFloat(width*0.5*100).toFixed(0.5)+"%");
+//       state=true;
+//   }
+// });
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -20,7 +20,7 @@ var modal = document.getElementById("myModal");
 var allimg = document.getElementsByClassName("abnormal");
 var modalImg = document.getElementById("img01");
 // var captionText = document.getElementById("caption");
-for (let i = 0; i < 25; i++){
+for (let i = 0; i < 50; i++){
   allimg[i].onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
